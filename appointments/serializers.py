acdf14cc2000831e4b1cc54e7c70c7f15e1fbfa6
Jaime <142696839+Jaime-D-Z@users.noreply.github.com>
2025-09-16 10:26:26 -0500

    Delete appointments directory

--- appointments/serializers.py
+++
@@ -1,8 +0,0 @@
-# appointments/serializers.py
-from rest_framework import serializers
-from .models import Appointment
-
-class AppointmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Appointment
-        fields = "__all__"
